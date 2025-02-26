bf540aeef39601fd2d3703b46b9618caf162ed7b
Sayman Lal <buisnesssayman@gmail.com>
2025-02-27 02:14:18 +0530

    Update urls.py

--- ArtSpot/urls.py
+++ ArtSpot/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 
 
@@ -11,5 +10,3 @@ urlpatterns = [
 ]
 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
